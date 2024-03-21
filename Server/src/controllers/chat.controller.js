763d67942760daf90ae04e0c8d5cd81c553e6330
Giriraj Singh Shekhawat <giriraj3103@gmail.com>
2024-03-21 07:22:50 +0530

    fetch all the chat controller

--- Server/src/controllers/chat.controller.js
+++ Server/src/controllers/chat.controller.js
@@ -3,7 +3,7 @@ import Chat from "../models/chat.model";
 import User from "../models/user.model";
 
 
-// accessing the chat for the particular user 
+// accessing the one to one chat of the user if it is not present there then create a new chat  
 
 const assessChat= async function(req,res){
   try {
@@ -63,7 +63,45 @@ const assessChat= async function(req,res){
 }
 
 
+// fetching all the chats for the loged in user 
+
+const fetchAllChats= async function(req,res){
+
+  try {
+    const  chat= await Chat.find({users:{$elemMatch:{$eq:req.user._id}}})
+                 .populate("users","-password")
+                 .populate("groupAdmin","-password")
+                 .populate("latestMessage")
+                 .sort({updatedAt:-1})
+                 .then(async (result)=>{
+                   result=await User.populate(result,{
+                    path:"latestMessage.sender",
+                    select:"name email avatar"
+                   })
+                 })
+
+             if(!chat){
+              res.status(400).json({
+                msg:"Error in fetching the chats",
+              })
+             }    
+             
+             res.status(200).json({
+              msg:"all chats are fetched successfully",
+              chat
+            })
+  } catch (error) {
+    console.log("error is from the fetch all the chats controller",error.message);
+    res.status(500).json({
+      msg:error.message
+    })
+  }
+}
+
+
+
 export {
     assessChat,
+    fetchAllChats,
 
 }
